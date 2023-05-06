7bf55320829028afeacc8119233a9618cd09dde0
Dammy <bestbynature2010@gmail.com>
2023-05-06 19:05:03 +0100

    :zap: Add the entry file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Fetchedusers from "./components/fetchedUsers";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <Fetchedusers />
+    </div>)
+      
+}
+
+export default App;
